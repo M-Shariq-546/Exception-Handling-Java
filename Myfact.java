532b493d03b9a4d64d1c6edd8bbc3257494eb398
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2023-08-02 21:18:51 +0500

    Create Myfact.java

---
+++ Myfact.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+interface MyTask extends Runnable {
+    void run();
+}
+
+class Myfact implements MyTask {
+    final long n;
+    public Myfact(long n){
+        this.n = n;
+    }
+    @Override
+    public void run() throws IllegalArgumentException {
+        if(n<0){
+            throw new  IllegalArgumentException("Factorial is not possible for Negative Number.");
+        }
+        else{
+            long factorial = 1;
+             for(int i=1; i<=n ; i++){
+                 factorial = factorial * i;
+             }
+             System.out.println("Factorial of a Given Number is: " + factorial);
+        }
+    }
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        
+        try{
+             System.out.print("Enter a non-negative integer to calculate its factorial: ");
+            int num = Integer.parseInt(scanner.nextLine());
+            
+            Myfact myobj = new Myfact(num);
+            myobj.run();
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid non-negative integer.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        scanner.close();
+    }
+}
